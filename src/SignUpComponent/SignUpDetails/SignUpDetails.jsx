6d21ba61297c2fabcf75af9036a5ada755174185
mahmudulhasanprodan <mahmudulhasan8627@gmail.com>
2025-04-13 23:09:54 +0600

    sign up part processing

--- src/SignUpComponent/SignUpDetails/SignUpDetails.jsx
+++ src/SignUpComponent/SignUpDetails/SignUpDetails.jsx
@@ -6,7 +6,7 @@ import Flex from '../../CommonComponent/Flex/Flex';
 const SignUpDetails = ({Title}) => {
 
   const[ShowInput,setShowInput]=useState({
-    FirstName: "",
+    FistName: "",
     LastName: "",
     EmailId: "",
     Number: "",
@@ -19,23 +19,26 @@ const SignUpDetails = ({Title}) => {
 
   const[ShowInputError,setShowInputError]=useState({
     FirstNameError: "",
+    LastNameError: "",
     EmailIdError: "",
     NumberError: "",
     AddressOneError: "",
     PasswordError: "",
     RepeatPasswordError: "",
     PasswordNotMatch: "",
+    CheckboxError: false,
   });
 
 // HandleChange Function Start Here
 const HandleChange = (e) =>{
-  console.log();
   if(e.target.checked){
     setShowInput({
+      ...ShowInput,
       [e.target.id]: true,
   })
   }else{
     setShowInput({
+      ...ShowInput,
       [e.target.id]: e.target.value,
     });
   }
@@ -46,95 +49,63 @@ const HandleChange = (e) =>{
 // HandleSignUp Function Start Here
 const HandleSignUp = () => {
     const {
-      FirstName,
+      FistName,
+      LastName,
       EmailId,
       Number,
       AddressOne,
       Password,
       RepeatPassword,
     } = ShowInput;
-
-    if (!FirstName) {
-      setShowInputError({
-        ...ShowInputError,
-        EmailIdError: "",
-        NumberError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "",
-        FirstNameError: "First name Missing",
-      });  
-    }else if(!EmailId){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        NumberError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "",
-        EmailIdError: "Email id Missing",
-      });    
-    }else if(!Number){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        EmailIdError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "",
-        NumberError: "Number Missing",
-      });   
-    }else if(!AddressOne){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        EmailIdError: "",
-        NumberError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "",
-        AddressOneError: "Address Missing",
-      });   
-    }else if(!Password){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        EmailIdError: "",
-        NumberError: "",
-        AddressOneError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "",
-        PasswordError: "Password Missing",
-      });  
-    }else if(!RepeatPassword){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        EmailIdError: "",
-        NumberError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        PasswordNotMatch: "",
-        RepeatPasswordError: "Repeat Password Missing",
-      }); 
-    }else if(Password !== RepeatPassword){
-      setShowInputError({
-        ...ShowInputError,
-        FirstNameError: "",
-        EmailIdError: "",
-        NumberError: "",
-        AddressOneError: "",
-        PasswordError: "",
-        RepeatPasswordError: "",
-        PasswordNotMatch: "Password Not Match",
-      }); 
-    }else{
-      console.log("everything is ok");    
-    }
+   if(!FistName){
+    setShowInputError({
+      ...ShowInputError,
+      FirstNameError: "First Name Missing",
+    })   
+   }else if(!LastName){
+    setShowInputError({
+      ...ShowInputError,
+      LastNameError: "Last Name Missing",
+    }) 
+   }else if(!EmailId){
+    setShowInputError({
+      ...ShowInputError,
+      EmailIdError: "Email Id Missing",
+    });
+   }else if(!Number){
+    setShowInputError({
+      ...ShowInputError,
+      NumberError: "Number Missing",
+    })
+   }else if(!AddressOne){
+    setShowInputError({
+      ...ShowInputError,
+      AddressOneError: "Address One Missing",
+    })
+   }else if(!Password){
+    setShowInputError({
+      ...ShowInputError,
+      PasswordError: "Password Missing",
+    })
+   }else if(!RepeatPassword){
+    setShowInputError({
+      ...ShowInputError,
+      RepeatPasswordError: "Repeat Password Missing",
+    })
+   }else if(Password !== RepeatPassword){
+    setShowInputError({
+      ...ShowInputError,
+      PasswordNotMatch: "Password Not Match",
+    })
+   }else if(!Checkbox){
+    setShowInputError({
+      ...ShowInputError,
+      CheckboxError: "Checkbox Not Check",
+    })
+   }else{
+    console.log("Everything is Ok");  
+   }
+  
 };
 
   return (
@@ -164,129 +135,122 @@ const HandleSignUp = () => {
                 Your Personals Destails
               </h2>
             </div>
-            <Flex className={"flex-col gap-y-8 px-4 xl:px-0"}>
-              <div className="flex flex-col md:flex-row gap-y-6 md:gap-y-0 gap-x-8 basis-1/2">
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"First Name*"}
-                    InputType={"text"}
-                    InputId={"FirstName"}
-                    InputName={"FirstName"}
-                    InputPlaceHolder={"Enter Your First Name Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                 
-                </div>
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Last Name*"}
-                    InputType={"text"}
-                    InputId={"LastName"}
-                    InputName={"LastName"}
-                    InputPlaceHolder={"Enter Your Last Name Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row gap-y-6 md:gap-y-0 gap-x-8 basis-1/2">
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Email*"}
-                    InputType={"email"}
-                    InputId={"EmailId"}
-                    InputName={"EmailId"}
-                    InputPlaceHolder={"Enter Your Email Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Phone Number"}
-                    InputType={"number"}
-                    InputId={"Number"}
-                    InputName={"Number"}
-                    InputPlaceHolder={"Enter Your Number Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row gap-y-6 md:gap-y-0 gap-x-8 basis-1/2">
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Address1*"}
-                    InputType={"text"}
-                    InputId={"AddressOne"}
-                    InputName={"AddressOne"}
-                    InputPlaceHolder={"Enter Your Address1 Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Address2"}
-                    InputType={"text"}
-                    InputId={"AddressTwo"}
-                    InputName={"AddressTwo"}
-                    InputPlaceHolder={"Enter Your Address2 Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row gap-y-6 md:gap-y-0 gap-x-8 basis-1/2">
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Password*"}
-                    InputType={"password"}
-                    InputId={"Password"}
-                    InputName={"Password"}
-                    InputPlaceHolder={"Enter Your Password Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-                <div className="basis-1/3">
-                  <SignForm
-                    LabelTitle={"Repeat Password*"}
-                    InputType={"password"}
-                    InputId={"RepeatPassword"}
-                    InputName={"RepeatPassword"}
-                    InputPlaceHolder={"Enter Your Repeat Password Here"}
-                    className={"border-b-[1px] border-b-slate-300 py-1"}
-                    OnchangeItem={HandleChange}
-                  />
-                </div>
-              </div>
-              <div className="flex items-center gap-x-3 mt-6">
-                <input
-                  type="checkbox"
-                  id="Checkbox"
-                  name="Checkbox"
-                  className="cursor-pointer"
-                  onChange={HandleChange}
-                />
-                <p className="font-DM_Sans text-sm md:text-base text-Btn_Color">
-                  I have read and agree to the Privacy and Policy
+          </div>
+          <Flex className={"items-center gap-x-8 gap-y-8 flex-wrap"}>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"First Name"}
+                InputType={"text"}
+                InputId={"FistName"}
+                InputName={"FistName"}
+                InputPlaceHolder={"Enter Your First Name"}
+                className={`${ShowInputError.FirstNameError ? "border-b-[1px] border-b-red-400 py-1" : "border-b-[1px] border-gray-300 py-1"}`}
+                OnchangeItem={HandleChange}
+              />
+
+              {ShowInputError.FirstNameError && (
+                <p className="text-red-500 font-DM_Sans">
+                  {ShowInputError.FirstNameError}
                 </p>
-              </div>
-              <div>
-                <button className="w-[200px] py-2 bg-blue-500 font-DM_Sans font-bold text-base text-Common_Color rounded-md" onClick={HandleSignUp}>
-                  Sign Up
-                </button>
-                {/* <button type="button" className="bg-indigo-500 w-[200px] px-10 py-2 flex items-center justify-center rounded-md" >
-                  <svg
-                    className="animate-spin h-5 w-5 mr-3 border-4 rounded-full border-t-4 border-green-500 border-b-white"
-                    viewBox="0 0 24 24"
-                  ></svg>
-                  Processing...
-                </button> */}
-              </div>
-            </Flex>
+              )}
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"Last Name"}
+                InputType={"text"}
+                InputId={"LastName"}
+                InputName={"LastName"}
+                InputPlaceHolder={"Enter Your Last Name"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"Email"}
+                InputType={"email"}
+                InputId={"EmailId"}
+                InputName={"EmailId"}
+                InputPlaceHolder={"Enter Your Email Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"Phone Number"}
+                InputType={"number"}
+                InputId={"Number"}
+                InputName={"Number"}
+                InputPlaceHolder={"Enter Your Number Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"AddressOne"}
+                InputType={"text"}
+                InputId={"AddressOne"}
+                InputName={"AddressOne"}
+                InputPlaceHolder={"Enter Your Address Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"AddressTwo"}
+                InputType={"password"}
+                InputId={"AddressTwo"}
+                InputName={"AddressTwo"}
+                InputPlaceHolder={"Enter Your Address Two Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"Password"}
+                InputType={"password"}
+                InputId={"Password"}
+                InputName={"Password"}
+                InputPlaceHolder={"Enter Your Password Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+            <div className="basis-1/3">
+              <SignForm
+                LabelTitle={"Repeat Password"}
+                InputType={"password"}
+                InputId={"RepeatPassword"}
+                InputName={"RepeatPassword"}
+                InputPlaceHolder={"Enter Your Repeat Password Here"}
+                className={"border-b-[1px] border-gray-300 py-1"}
+                OnchangeItem={HandleChange}
+              />
+            </div>
+          </Flex>
+          <div className="flex items-center gap-x-3 mt-10">
+            <input
+              type="checkbox"
+              id="Checkbox"
+              name="Checkbox"
+              className="cursor-pointer"
+              onClick={HandleChange}
+            />
+            <p className="font-DM_Sans text-base text-Text_Color">
+              I have read and agree to the privacy policy
+            </p>
+          </div>
+          <div>
+            <button
+              className="w-48 py-2 bg-Btn_Color mt-10 text-white font-bold"
+              onClick={HandleSignUp}
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
